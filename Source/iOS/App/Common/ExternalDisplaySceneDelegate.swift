29d12bdb011dad47278ba2638981589a914136fc
Felix Groove <felix.groove@lhind.dlh.de>
2025-07-04 09:35:35 +0200

    external display fix

--- Source/iOS/App/Common/ExternalDisplaySceneDelegate.swift
+++ Source/iOS/App/Common/ExternalDisplaySceneDelegate.swift
@@ -7,6 +7,22 @@ class ExternalDisplaySceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
   
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    
+    // Create window explicitly to configure overscan compensation
+    window = UIWindow(windowScene: windowScene)
+    
+    // Fix black edges on external monitor by disabling overscan compensation
+    window?.screen.overscanCompensation = .none
+    
+    // Load the external display storyboard and set up the initial view controller
+    let storyboard = UIStoryboard(name: "ExternalDisplay", bundle: nil)
+    if let initialViewController = storyboard.instantiateInitialViewController() {
+      window?.rootViewController = initialViewController
+    }
+    
+    window?.makeKeyAndVisible()
+    
     EmulationCoordinator.shared().isExternalDisplayConnected = true
   }
   
